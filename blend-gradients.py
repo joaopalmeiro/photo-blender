74125acbe3a8ce7ff2bd510c936588820a7de492
João Palmeiro <joaommpalmeiro@gmail.com>
2023-05-09 21:36:34 +0100

    Closes #6

--- blend-gradients.py
+++ blend-gradients.py
@@ -4,23 +4,32 @@
 # https://stackoverflow.com/a/50557495
 # https://pillow.readthedocs.io/en/stable/reference/ImageFilter.html:
 # - FIND_EDGES for edge detection
+# https://docs.python.org/3.7/library/pathlib.html#pathlib.Path.glob
 
-import glob
+from pathlib import Path
 
 from PIL import Image
 
-PHOTO_IMG: str = "photos/IMG_4492-4.jpg"
-GRADIENT_FOLDER: str = "gradients/"
-OUTPUT_FOLDER: str = "output"
+PHOTO_FOLDER: Path = Path("photos/")
+PHOTO_IMG: Path = PHOTO_FOLDER / "IMG_4492-4.jpg"
+GRADIENT_FOLDER: Path = Path("gradients/")
+OUTPUT_FOLDER: Path = Path("output/")
 
 ALPHA: float = 0.3
 
 if __name__ == "__main__":
     with Image.open(PHOTO_IMG) as photo:
-        for f in glob.iglob(f"{GRADIENT_FOLDER}/*.png"):
-            print(f)
-            with Image.open(f) as gradient:
+        # for f in glob.iglob(f"{GRADIENT_FOLDER}/*.png"):
+        for gradient_path in GRADIENT_FOLDER.glob("*.png"):
+            print(f"Gradient: {gradient_path}")
+            with Image.open(gradient_path) as gradient:
                 output = Image.blend(photo, gradient, ALPHA)
-                output.save(f"{OUTPUT_FOLDER}/output.jpg")
+
+                output_suffix = gradient_path.stem
+                output_path = OUTPUT_FOLDER / f"output-{output_suffix}.jpg"
+
+                output.save(output_path)
+
+                print(f"Output: {output_path}", end="\n" * 2)
 
     print("Done!")
--- gradients/README.md
+++ gradients/README.md
@@ -5,3 +5,5 @@
 - `135099l-6x6.png`:
   - https://myanimelist.net/anime/51019/Kimetsu_no_Yaiba__Katanakaji_no_Sato-hen/pics
   - https://cdn.myanimelist.net/images/anime/1765/135099l.jpg
+- `121027l-6x6.png`
+  - https://cdn.myanimelist.net/images/anime/1499/121027l.jpg
